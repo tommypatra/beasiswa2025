ca7e33ead7d68a904819beb7d02c6569ad46c67b
tommypatra <tommyirawan.patra@gmail.com>
2025-08-28 10:09:56 +0800

    peserta survei

--- resources/views/app/peserta_survei.blade.php
+++ resources/views/app/peserta_survei.blade.php
@@ -846,7 +846,7 @@
 
         async function load_data_peserta_survei(){
             let peserta_survei = await asyncFunction(`${base_url}/api/peserta-survei/${pendaftar_id}`);
-            survei_peserta = peserta_survei.data.survei_peserta[0];            
+            survei_peserta = peserta_survei.data.survei_peserta;            
             // console.log(survei_peserta);
             if (survei_peserta.hasil!=null) {
                 disabledForm(true);            
