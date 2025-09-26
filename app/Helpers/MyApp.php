6840871c6171635663a6d94b7436df77303b64c8
tommypatra <tommyirawan.patra@gmail.com>
2025-09-27 04:29:55 +0800

    validasi orang tua

--- app/Helpers/MyApp.php
+++ app/Helpers/MyApp.php
@@ -155,8 +155,7 @@ if (!function_exists('validasiPendaftaran')) {
         $data['pendidikan_akhir'] = true;
 
         if ($beasiswa->perlu_data_orang_tua) {
-            // $data['orang_tua'] = ($user->orangTua) ? true : false;
-            $data['orang_tua'] = ($user->orangTua && !empty($user->orangTua->tanggungan));
+            $data['orang_tua'] = ($user->orangTua && ($user->orangTua->tanggungan >= 0));
         }
 
         if ($beasiswa->perlu_data_nilai_raport) {
